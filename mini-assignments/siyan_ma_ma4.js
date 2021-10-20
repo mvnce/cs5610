d9390610b38f75d88e96c6ea13c6f6f298d90827
Vincent Ma <mvnce@amazon.com>
2021-10-20 03:03:53 -0700

    Updated MA4 format (linted)

--- mini-assignments/siyan_ma_ma4.js
+++ mini-assignments/siyan_ma_ma4.js
@@ -10,11 +10,11 @@
     an integer representing the biggest number in int.  
     if there is no valid output, return undefined 
   note: I have provided some code within the function to help you out
-
 */
 function findBiggestNumber(map) {
-  if (map == null || map.length === 0 || map[0].length === 0)
+  if (map == null || map.length === 0 || map[0].length === 0) {
     return undefined;
+  }
 
   let biggestNumber = map[0][0];
 
@@ -41,7 +41,6 @@ output
   true if the number of all the characters in the string is the same
   otherwise, false
 */
-
 function balancedString(str) {
   if (str == null) {
     return false;
@@ -101,36 +100,35 @@ function additivePersistence(num) {
   return count;
 }
 
-
 // TEST 1 - findBiggestNumber
 const grid1 = [
   [1, 2, 3, 4, 5, 6],
   [7, 8, 9, 10, 11, 12]
-]
+];
 
 const grid2 = [
   [1, 1, 4, 1],
   [1, 6],
   [1, 2, 1, -3],
-]
+];
 
 const grid3 = [
   [1, null, 1, null],
   [1, 0],
   [1, 2, 1, undefined],
-]
+];
 
 const grid4 = [
   [-1, null],
   [],
   [0, -2, -1],
-]
+];
 
 const grid5 = [
   [],
   [],
   [],
-]
+];
 
 console.assert(findBiggestNumber(grid1) === 12, "biggest number should be 12");
 console.assert(findBiggestNumber(grid2) === 6, "biggest number should be 6");
@@ -138,24 +136,22 @@ console.assert(findBiggestNumber(grid3) === 2, "biggest number should be 2");
 console.assert(findBiggestNumber(grid4) === 0, "biggest number should be 0");
 console.assert(findBiggestNumber(grid5) === undefined, "biggest number response should be undefined");
 
-
 // TEST 2 - balancedString
-console.assert(balancedString("xxxyyyzzz") === true, "'xxxyyyzzz' is balanced")
-console.assert(balancedString("xxxyyyzzzz") === false, '"xxxyyyzzzz" is not balanced')
-console.assert(balancedString("abccbaabccba") === true, '"abccbaabccba" is balanced')
-console.assert(balancedString("abcdefghijklmnopqrstuvwxyz") === true, '"abcdefghijklmnopqrstuvwxyz" is balanced')
-console.assert(balancedString("pqq") === false, '"pqq" is not balanced')
-console.assert(balancedString("fdedfdeffeddefeeeefddf") === false, '"fdedfdeffeddefeeeefddf" is not balanced')
-console.assert(balancedString("www") === true, '"www is balanced')
-console.assert(balancedString("x") === true, '"x" is balanced')
-console.assert(balancedString("") === true, "'' is balanced")
-
+console.assert(balancedString("xxxyyyzzz") === true, "'xxxyyyzzz' is balanced");
+console.assert(balancedString("xxxyyyzzzz") === false, '"xxxyyyzzzz" is not balanced');
+console.assert(balancedString("abccbaabccba") === true, '"abccbaabccba" is balanced');
+console.assert(balancedString("abcdefghijklmnopqrstuvwxyz") === true, '"abcdefghijklmnopqrstuvwxyz" is balanced');
+console.assert(balancedString("pqq") === false, '"pqq" is not balanced');
+console.assert(balancedString("fdedfdeffeddefeeeefddf") === false, '"fdedfdeffeddefeeeefddf" is not balanced');
+console.assert(balancedString("www") === true, '"www is balanced');
+console.assert(balancedString("x") === true, '"x" is balanced');
+console.assert(balancedString("") === true, "'' is balanced");
 
 // TEST 3 - additivePersistence
-console.assert(additivePersistence(1234) === 2, 'the additive 1234 should be 2')
-console.assert(additivePersistence(13) === 1, 'the additive 13 should be 1')
-console.assert(additivePersistence(9876) === 2, 'the additive 9876 should be 2')
-console.assert(additivePersistence(199) === 3, 'the additive 199 should be 3')
-console.assert(additivePersistence(1679583) === 3, 'the additive 1679583 should be 3')
+console.assert(additivePersistence(1234) === 2, 'the additive 1234 should be 2');
+console.assert(additivePersistence(13) === 1, 'the additive 13 should be 1');
+console.assert(additivePersistence(9876) === 2, 'the additive 9876 should be 2');
+console.assert(additivePersistence(199) === 3, 'the additive 199 should be 3');
+console.assert(additivePersistence(1679583) === 3, 'the additive 1679583 should be 3');
 
-console.log("code execution complete!")
+console.log("code execution complete!");
